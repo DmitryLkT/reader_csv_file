3fb65d84ba0cbcbee19814e228edcf30183272fd
DmitryTL <Dmitry.plus1@yandex.ru>
2025-11-21 17:42:55 +0600

    fix file_reader.py

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -18,25 +18,4 @@ def reader(*files: str):
                 selected = [row[i] for i in ind]
                 data.append(selected)
 
-    sums = {}
-    counts = {}
-
-    for position, performance in data:
-        performance = float(performance)
-
-        if position not in sums:
-            sums[position] = performance
-            counts[position] = 1
-        else:
-            sums[position] += performance
-            counts[position] += 1
-
-    result = []
-
-    for position in sums:
-        avg = sums[position] / counts[position]
-        result.append([position, round(avg, 2)])
-
-    result.sort(key=lambda x: x[1], reverse=True)
-
-    return result
+    return data
