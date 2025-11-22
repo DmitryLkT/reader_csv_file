a93013d5ef9cbbaa8d80ba718bc702c9fa3b0f86
DmitryTL <Dmitry.plus1@yandex.ru>
2025-11-22 15:18:04 +0600

    add test_performance.py

--- reports/save_file.py
+++ reports/save_file.py
@@ -1,5 +1,21 @@
 import csv
 
+def validate_data_save_file(func):
+    def wrapper(filename: str, rows: list[dict]):
+        if not rows:
+            raise ValueError("Empty list of lines")
+
+        if not rows[0]:
+            raise ValueError("Empty dict")
+
+        if len(rows) > 2:
+            if rows[1].keys() != rows[2].keys():
+                raise ValueError("Keys do not match")
+
+        return func(filename, rows)
+    return wrapper
+
+@validate_data_save_file
 def save(filename, rows):
     if not filename.endswith('.csv'):
         filename += '.csv'
